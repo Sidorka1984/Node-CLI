7b8da3ebc90a5697c478034a2e6d2c17faf2e385
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-09-13 16:48:31 +0300

    done

---
+++ index.js
@@ -0,0 +1,43 @@
+const contactsOperations = require("./contacts.js");
+
+const workWithContacts = async (type = 'listContacts', id, name, email, phone) => {
+    try {
+        switch (type) {
+            case 'listContacts':
+                return await contactsOperations.listContacts();
+            case 'getContactById':
+                return await contactsOperations.getContactById(id);
+            case 'removeContact':
+                return await contactsOperations.removeContact(id);
+            case 'addContact':
+                return await contactsOperations.addContact( name, email, phone );
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+// workWithContacts('listContacts')
+    // .then(data => console.table(data))
+    // .catch(error => console.table(error))
+
+// workWithContacts('getContactById', 7)
+//  .then(data => console.log(data))
+//  .catch(error => console.log(error))
+// 
+// workWithContacts('removeContact', 7)
+    // .then(data => console.table(data))
+    // .catch(error => console.table(error))
+// 
+const newData = {
+    name: 'Diana Dinisova',
+    email: 'diana@gmail.com',
+    phone: '0990009999'
+}
+workWithContacts('addContact', '', newData)
+    .then(data => console.table(data))
+    .catch(error => console.table(error))
+
+
+
+
